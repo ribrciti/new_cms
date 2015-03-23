411353ba03e6b63d1a21741442e3b2911c99a9bd
ribrciti <rbciti@msn.com>
2015-03-23 07:05:39 -0500

    first commit plus demo controller and views

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,29 @@
+class DemoController < ApplicationController
+
+	layout false  #'application'
+
+  def index
+	end
+
+  def hello
+		#render('index')
+		@array = [1,2,3,4,5]
+		@id = params['id'].to_i
+		@page = params[:page].to_i
+  end
+
+  def other_hello
+		redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def test_url
+  	redirect_to("http://msn.com")  	
+  end
+  
+  def text_helpers  	  	
+  end
+
+  def escape_output  	
+  end
+
+end
